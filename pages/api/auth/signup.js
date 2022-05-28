c9f1642a156bd2c240d78d4275b872d6d63f5af3
Anderson Pablo <anderson.pablo02@gmail.com>
2022-05-27 23:23:34 -0300

    send data the user to mongoDb

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,38 +1,42 @@
-import { hashPassword } from "../../../lib/auth";
-import { connectToDataBase } from "../../../lib/db";
+import { hashPassword } from '../../../lib/auth';
+import { connectToDatabase } from '../../../lib/db';
 
 async function handler(req, res) {
-
-  const data = req.body;
-
-  const { email, password } = data;
-
-  if (!email ||
-    !email.includes('@') ||
-    !password ||
-    !password.trim().length < 7) {
-    res.status(422)
-      .json({
-        message:
-          'invalids inputs'
-      });
-
+  if (req.method !== 'POST') {
     return;
   }
 
-  const client = await connectToDataBase();
+  const data = req.body;
+
+
+
+  const { email, password } = data;
+
+  if (
+    !email ||
+    !email.includes('@') ||
+    !password ||
+    password.trim().length < 7
+  ) {
+    res.status(422).json({
+      message:
+        'Invalid input - password should also be at least 7 characters long.',
+    });
+    return;
+  }
+
+  const client = await connectToDatabase();
 
   const db = client.db();
 
-  const hashedPassword = hashPassword(password);
+  const hashedPassword = await hashPassword(password);
 
   const result = await db.collection('users').insertOne({
     email: email,
-    password: hashedPassword
+    password: hashedPassword,
   });
 
-  res.status(200).json({ message: 'created user!' });
-
+  res.status(201).json({ message: 'Created user!' });
 }
 
 export default handler;
